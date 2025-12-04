c010d4412532a3c22aba3d1d2a7e98656edefa2a
supergate-jhbyun <jh.byun@supergate.cc>
2025-12-04 15:58:19 +0900

    feat: add Slurm REST API v0.0.40 generated client
    Signed-off-by: supergate-jhbyun <jh.byun@supergate.cc>

---
+++ api/v0040/api.go
@@ -0,0 +1,5 @@
+package v0040
+
+// Ref: https://slurm.schedmd.com/rest_api.html
+
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@v2.4.1 -config oapi-codegen-config.yaml slurm-openapi.gen.json
